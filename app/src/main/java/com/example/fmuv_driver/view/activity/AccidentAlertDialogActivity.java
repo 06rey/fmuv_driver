8a0e19b089db727ff655d7b45973ad5341219d8a
06rey <junrey.algario@gmail.com>
2020-03-12 23:25:51 +0800

    Feedback added

---
+++ app/src/main/java/com/example/fmuv_driver/view/activity/AccidentAlertDialogActivity.java
@@ -0,0 +1,14 @@
+package com.example.fmuv_driver.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccidentAlertDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accident_alert_dialog);
+    }
+}
